c4a12afcad10d16a222dade77fc1153e53325438
Muhammad Azad <muhammad.barznji97@gmail.com>
2024-02-02 10:23:39 +0300

    Add Example and update gitignore

---
+++ ScrollViewRTLExample/ScrollViewRTLExample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//
+// ContentView.swift
+// ScrollViewRTLExample
+//
+// Created by Muhammad Azad on 02/02/2024
+// Copyright © 2024 RIGT. All rights reserved.
+//
+        
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
